ff99c3d6d09cedfe9af2e2817a144ad82e38c416
santije23 <santiago.jimeneze@udea.edu.co>
2023-10-17 22:24:57 -0500

    Se agrega el proyecto

---
+++ src/main/java/org/santiago/model/PersonAttributes.java
@@ -0,0 +1,6 @@
+package org.santiago.model;
+
+public record PersonAttributes(String name, String lastName, Integer age, Character gender,
+                               boolean employmentStatus, Integer stratum, Integer numberOfChildren,
+                               String educationalLevel, Double salary) {
+}
